89e8305904e8a1c0a526ae9815d0a971c740cff8
Nathan <hello@nathandorney.co.uk>
2023-03-12 15:57:00 +0000

    did it fix itself?

---
+++ pages/page.js
@@ -0,0 +1,16 @@
+import Guide from "../components/Guide";
+
+export default function Home() {
+  return (
+    <>
+      <div className="layout">
+        <h1>V60 Recipe</h1>
+        <Guide />
+
+        <a href="https://read.cv/nathan" className="note">
+          Made by @nathan
+        </a>
+      </div>
+    </>
+  );
+}
